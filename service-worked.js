e866383eedca76b25461799e8d07c1a8363f2728
PMGonzaga <104696900+PMGonzaga@users.noreply.github.com>
2026-05-16 12:14:14 -0300

    Update service worker cache configuration and files

--- service-worked.js
+++ service-worked.js
@@ -1,29 +1,57 @@
-const CACHE_NAME = 'lista-chamada-v1';
+const CACHE_NAME = "chamada-escola-v1";
 
 const urlsToCache = [
-    '/',
-    '/index.html',
-    '/dashboard.html',
-    '/alunos.html',
-    '/chamada.html',
-    '/css/style.css'
+
+    "./",
+
+    "./index.html",
+
+    "./dashboard.html",
+
+    "./alunos.html",
+
+    "./chamada.html",
+
+    "./historico.html",
+
+    "./resultado-historico.html",
+
+    "./css/style.css",
+
+    "./js/app.js",
+
+    "./js/auth.js",
+
+    "./js/firebase.js",
+
+    "./js/alunos.js",
+
+    "./js/chamada.js",
+
+    "./js/historico.js",
+
+    "./js/resultado-historico.js"
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener("install", (event) => {
 
     event.waitUntil(
+
         caches.open(CACHE_NAME)
-        .then(cache => {
+        .then((cache) => {
+
             return cache.addAll(urlsToCache);
         })
     );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", (event) => {
 
     event.respondWith(
+
         caches.match(event.request)
-        .then(response => {
+        .then((response) => {
+
             return response || fetch(event.request);
         })
     );
