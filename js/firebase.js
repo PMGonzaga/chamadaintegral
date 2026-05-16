a05b15cfc1faf902f92972f8cf9cae2d886d08c8
PMGonzaga <104696900+PMGonzaga@users.noreply.github.com>
2026-05-15 22:26:31 -0300

    edit code

--- js/firebase.js
+++ js/firebase.js
@@ -1,21 +1,33 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp }
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { getAuth }
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+import { getFirestore }
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDpkpoabYifjVAqcand-GyHedFymxkt7IQ",
-  authDomain: "lista-chamada-escola.firebaseapp.com",
-  projectId: "lista-chamada-escola",
-  storageBucket: "lista-chamada-escola.firebasestorage.app",
-  messagingSenderId: "712228308178",
-  appId: "1:712228308178:web:0beb3c9eb6727643dd590b",
-  measurementId: "G-0MYRDXM13W"
+
+    apiKey: "AIzaSyDpkpoabYifjVAqcand-GyHedFymxkt7IQ",
+
+    authDomain: "lista-chamada-escola.firebaseapp.com",
+
+    projectId: "lista-chamada-escola",
+
+    storageBucket: "lista-chamada-escola.firebasestorage.app",
+
+    messagingSenderId: "712228308178",
+
+    appId: "1:712228308178:web:0beb3c9eb6727643dd590b",
+
+    measurementId: "G-0MYRDXM13W"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+const auth = getAuth(app);
+
+const db = getFirestore(app);
+
+export { auth, db };
