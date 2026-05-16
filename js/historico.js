51600b90d345d5e038a3811d19f7695d283478a4
PMGonzaga <104696900+PMGonzaga@users.noreply.github.com>
2026-05-16 12:05:57 -0300

    Add buscarHistorico function to manage history

---
+++ js/historico.js
@@ -0,0 +1,18 @@
+function buscarHistorico() {
+
+    const colete = document.getElementById("colete").value;
+
+    const dataInicial = document.getElementById("data-inicial").value;
+
+    const dataFinal = document.getElementById("data-final").value;
+
+    localStorage.setItem("historicoColete", colete);
+
+    localStorage.setItem("historicoDataInicial", dataInicial);
+
+    localStorage.setItem("historicoDataFinal", dataFinal);
+
+    window.location.href = "resultado-historico.html";
+}
+
+window.buscarHistorico = buscarHistorico;
